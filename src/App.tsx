cbfcda4cda31fab456940fcb065dc1fa18a5646f
venancioalves <venaciobsn@gmail.com>
2024-03-05 18:26:32 -0300

    Commit Initial ✅

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.scss'
+
+function App() {
+  return (
+    <main>
+      <header className='header-container'>
+          <img src="./logo.png" alt="" className='logo-img'/> 
+          <nav className='menu-options'>
+            <a href="">Sobre nós</a>
+            <a href="">Como Funcionamos</a>
+            <a href="">Fornacedores</a>
+          </nav>
+          <button>Contate-nos</button>
+      </header>
+    </main>
+  )
+}
+
+export default App
